ee5ece232eede4b6f94a363e320fa7e9c85d4b64
SOLEDADGONZALEZ06 <soledadgonzalez06@gmail.com>
2026-09-20 21:28:43 -0300

    fix: aumentar timeout y usar visibility en login_page para CI

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -2,8 +2,6 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from tests.conftest import driver
-
 PACKAGE = "com.saucelabs.mydemoapp.android"
 
 class LoginPage:
@@ -14,29 +12,27 @@ class LoginPage:
     PASSWORD_ERROR = (AppiumBy.ID, f"{PACKAGE}:id/passwordErrorTV")
 
     def enter_username(self, driver, usuario):
-        campo = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(self.USERNAME_INPUT)
-     )
+        campo = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located(self.USERNAME_INPUT)
+        )
         campo.clear()
         campo.send_keys(usuario)
 
     def enter_password(self, driver, password):
-        campo = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(self.PASSWORD_INPUT)
-    )
+        campo = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located(self.PASSWORD_INPUT)
+        )
         campo.clear()
         campo.send_keys(password)
 
     def tap_login(self, driver):
-        boton = WebDriverWait(driver, 10).until(
+        boton = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable(self.LOGIN_BUTTON)
         )
         boton.click()
 
     def get_password_error_text(self, driver):
-        elemento = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located(self.PASSWORD_ERROR)
+        elemento = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located(self.PASSWORD_ERROR)
         )
         return elemento.text
-
-   
